fd310ab5e29ff0c1a4b2a142ed3b248816258492
Alexander Kosmehl <kosmehl@live.de>
2025-12-02 18:01:36 +0100

    Updated formatting rules of old code

---
+++ day-1/Day1.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest"
+import { readFileSync } from "node:fs"
+import { solvePart1, solvePart2 } from "./Day1"
+
+
+describe("Day 1", () => {
+    const sample = readFileSync(new URL("./sample.txt", import.meta.url), "utf8").split(/\r?\n/)
+    const input = readFileSync(new URL("./input.txt", import.meta.url), "utf8").split(/\r?\n/)
+
+    describe("Part 1", () => {
+        it("should solve the sample input", () => {
+            expect(solvePart1(sample)).toBe(3)
+        })
+
+        it("should solve the actual input", () => {
+            const result = solvePart1(input)
+            console.log("Day 1 - Part 1: ", result)
+
+            expect(result).toBe(1021)
+        })
+    })
+
+    describe("Part 2", () => {
+        it("should solve the sample input", () => {
+            expect(solvePart2(sample)).toBe(6)
+        })
+
+        it("should solve the actual input", () => {
+            const result = solvePart2(input)
+            console.log("Day 1 - Part 2: ", result)
+
+            expect(result).toBe(5933)
+        })
+    })
+})
---
+++ day-1/Day1.ts
@@ -0,0 +1,70 @@
+export function solvePart1(input: string[]): number {
+    let count = 0
+
+    let currentPosition = 50
+
+    for (const line of input) {
+        const direction = line[0]
+        const distance = parseInt(line.substring(1))
+
+        if (direction === "L") {
+            const newPosition = (currentPosition - distance) % 100
+            currentPosition = newPosition < 0 ? 100 - Math.abs(newPosition) : newPosition
+        } else if (direction === "R") {
+            const newPosition = (currentPosition + distance) % 100
+            currentPosition = newPosition > 0 ? 0 + Math.abs(newPosition) : newPosition
+        }
+
+        if (currentPosition === 0) {
+            count += 1
+        }
+    }
+
+    return count
+}
+
+export function solvePart2(input: string[]): number {
+    let count = 0
+
+    let currentPosition = 50
+
+    for (const line of input) {
+        const direction = line[0]
+        const distance = parseInt(line.substring(1))
+
+        if (direction === "L") {
+            let newPosition = currentPosition - distance
+            while (newPosition < 0) {
+                count += 1
+                newPosition += 100
+            }
+
+            // special case that would get counted twice
+            if (currentPosition === 0) {
+                count -= 1
+            }
+            currentPosition = newPosition
+
+        } else if (direction === "R") {
+            let newPosition = currentPosition + distance
+
+            while (newPosition > 100) {
+                count += 1
+                newPosition -= 100
+            }
+
+            // special case that would get counted twice
+            if (newPosition === 100) {
+                newPosition = 0
+            }
+
+            currentPosition = newPosition
+        }
+
+        if (currentPosition === 0) {
+            count += 1
+        }
+    }
+
+    return count
+}
