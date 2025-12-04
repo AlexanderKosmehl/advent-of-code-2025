e34746c137479f96c2eb2648290a86611b3c2e33
Alexander Kosmehl <kosmehl@live.de>
2025-12-04 21:46:33 +0100

    Added day 3 solution

---
+++ day-3/Day3.test.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from "fs"
+import { describe, expect, it } from "vitest"
+import { solvePart1, solvePart2 } from "./Day3"
+
+describe("Day 3", () => {
+    const sample = readFileSync(new URL("./sample.txt", import.meta.url), "utf8").split(/\r?\n/)
+    const input = readFileSync(new URL("./input.txt", import.meta.url), "utf8").split(/\r?\n/)
+
+    describe("Part 1", () => {
+        it("should solve the sample input", () => {
+            expect(solvePart1(sample)).toBe(357)
+        })
+
+        it("should solve the actual input", () => {
+            const result = solvePart1(input)
+            console.log("Day 2 - Part 1: ", result)
+
+            expect(result).toBeDefined()
+        })
+    })
+
+    describe("Part 2", () => {
+        it("should solve the sample input", () => {
+            expect(solvePart2(sample, 12)).toBe(3121910778619)
+        })
+
+        it("should match part 1 solution for chain length 2", () => {
+            expect(solvePart2(sample, 2)).toBe(solvePart1(sample))
+        })
+
+        it("should solve the actual input", () => {
+            const result = solvePart2(input, 12)
+            console.log("Day 2 - Part 1: ", result)
+
+            expect(result).toBeDefined()
+        })
+    })
+})
---
+++ day-3/Day3.ts
@@ -0,0 +1,57 @@
+function getMaximumJoltageForBank(bankString: string): number {
+    let maxJoltage = 0
+
+    for (let firstIndex = 0; firstIndex < bankString.length - 1; firstIndex++) {
+        for (let secondIndex = firstIndex + 1; secondIndex < bankString.length; secondIndex++) {
+            const joltage = Number(bankString.charAt(firstIndex) + bankString.charAt(secondIndex))
+            if (joltage > maxJoltage) {
+                maxJoltage = joltage
+            }
+        }
+    }
+
+    return maxJoltage
+}
+
+function getMaximumJoltageForBank2(bankString: string, chainLength: number): number {
+    // 0 length -> 0 joltage
+    if (chainLength <= 0) return 0
+
+    // chain length => bank -> take all digits
+    if (chainLength >= bankString.length) return Number(bankString)
+
+    let startIndex = 0
+    let optimalBankString = ""
+
+    for (let picked = 0; picked < chainLength; picked++) {
+        const remainingPicks = chainLength - picked
+        const lastPossibleIndex = bankString.length - remainingPicks
+        let bestDigit = -1
+        let bestIndex = startIndex
+
+        for (let currentIndex = startIndex; currentIndex <= lastPossibleIndex; currentIndex++) {
+            const currentDigit = Number(bankString.charAt(currentIndex))
+
+            if (currentDigit > bestDigit) {
+                bestDigit = currentDigit
+                bestIndex = currentIndex
+
+                // exit early if we are already using the best possible digit
+                if (bestDigit === 9) break
+            }
+        }
+
+        optimalBankString += String(bestDigit)
+        startIndex = bestIndex + 1
+    }
+
+    return Number(optimalBankString)
+}
+
+export function solvePart1(input: string[]): number {
+    return input.reduce((joltageSum, batteryBankString) => joltageSum + getMaximumJoltageForBank2(batteryBankString, 2), 0)
+}
+
+export function solvePart2(input: string[], chainLength: number): number {
+    return input.reduce((joltageSum, batteryBankString) => joltageSum + getMaximumJoltageForBank2(batteryBankString, chainLength), 0)
+}   
